a86180ecb26b1467cf8bb40bf150e29ae4f4fb2c
jyt6640 <jyt6640@gmail.com>
2025-11-06 16:49:14 +0900

    feat(RequestBodyBuilder): 요청 객체를 JSON 문자열로 변환 기능 구현

--- src/main/java/TeDDie/api/RequestBodyBuilder.java
+++ src/main/java/TeDDie/api/RequestBodyBuilder.java
@@ -1,5 +1,6 @@
 package TeDDie.api;
 
+import com.google.gson.Gson;
 import java.util.List;
 
 public class RequestBodyBuilder {
@@ -7,6 +8,12 @@ public class RequestBodyBuilder {
     private static final String USER_ROLE = "user";
     private static final double DEFAULT_TEMPERATURE = 0.7;
     private static final int DEFAULT_MAX_TOKENS = 2000;
+
+    private final Gson gson;
+
+    public RequestBodyBuilder() {
+        this.gson = new Gson();
+    }
 
     public ApiRequest buildRequestObject(String prompt) {
         ApiMessage message = new ApiMessage(USER_ROLE, prompt);
@@ -16,5 +23,10 @@ public class RequestBodyBuilder {
                 DEFAULT_TEMPERATURE,
                 DEFAULT_MAX_TOKENS
         );
+    }
+
+    public String createJSONBody(String prompt) {
+        ApiRequest request = buildRequestObject(prompt);
+        return gson.toJson(request);
     }
 }
