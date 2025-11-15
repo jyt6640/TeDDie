f6a37d2f0543ef2cd89de826b4f0d9dfbfa169f9
jyt6640 <jyt6640@gmail.com>
2025-11-15 21:38:29 +0900

    feat(ApiResponse): Message가 비어있으면 예외 발생 기능 구현

--- src/main/java/TeDDie/service/ApiResponse.java
+++ src/main/java/TeDDie/service/ApiResponse.java
@@ -16,6 +16,16 @@ record ApiResponse(List<Choice> choices) {
 }
 
 record Choice(Message message) {
+    Choice {
+        validateEmpty(message);
+    }
+
+    private void validateEmpty(Message message) {
+        if (message == null) {
+            throw new IllegalArgumentException("[ERROR] API 응답에 message가 없습니다.");
+        }
+    }
+
     String extractMessage() {
         return message.extractContent();
     }
