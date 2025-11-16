0b4004aa4389657d6ec0474a2e0965f2a980e468
jyt6640 <jyt6640@gmail.com>
2025-11-16 17:36:10 +0900

    feat(UserPrompt): 프롬프트를 포함하여 생성하는 기능 구현

--- src/main/java/TeDDie/domain/UserPrompt.java
+++ src/main/java/TeDDie/domain/UserPrompt.java
@@ -1,13 +1,22 @@
 package TeDDie.domain;
 
 public class UserPrompt {
+    private static final String TEMPLATE = """
+            - 주제: %s
+            - 난이도: %s
+            """;
+
     private final String content;
 
     public UserPrompt(Topic topic, Difficulty difficulty) {
         this.content = String.format(
+                TEMPLATE,
                 topic.getValue(),
                 difficulty.getValue()
         );
     }
 
+    public String getContent() {
+        return content;
+    }
 }
