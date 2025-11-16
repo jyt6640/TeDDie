47652a08adeb309a41642f8a3102ecc0e55e81e1
jyt6640 <jyt6640@gmail.com>
2025-11-16 17:35:53 +0900

    test(UserPromptTest): 프롬프트를 포함하여 생성 테스트 추가

--- src/test/java/TeDDie/domain/UserPromptTest.java
+++ src/test/java/TeDDie/domain/UserPromptTest.java
@@ -1,5 +1,6 @@
 package TeDDie.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 import org.junit.jupiter.api.DisplayName;
@@ -16,5 +17,22 @@ public class UserPromptTest {
         //when&then
         assertThatCode(() -> new UserPrompt(topic, difficulty))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("프롬프트를 포함하여 생성")
+    @Test
+    void 프롬프트를_포함하여_생성() {
+        //given
+        Topic topic = new Topic("random");
+        Difficulty difficulty = Difficulty.from("easy");
+
+        //when
+        UserPrompt result = new UserPrompt(topic, difficulty);
+
+        //then
+        assertThat(result.getContent()).isEqualTo("""
+                - 주제: random
+                - 난이도: easy
+                """);
     }
 }
